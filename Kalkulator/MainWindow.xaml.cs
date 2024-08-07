4cc8275e309b907007c4744dba4cd64ce07289d7
Dawid Krafczyk <dawidkrafddd@gmail.com>
2024-08-07 15:24:35 +0200

    Dodanie zdarzenia kliknięcia przycisków

--- Kalkulator/MainWindow.xaml.cs
+++ Kalkulator/MainWindow.xaml.cs
@@ -24,5 +24,95 @@ namespace Kalkulator
         {
             InitializeComponent();
         }
+
+        private void zero_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void one_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void two_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void three_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void four_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void five_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void six_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void seven_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void eight_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void nine_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void dot_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void equal_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void plus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void minus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void multiply_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void divide_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void backspace_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void clear_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
